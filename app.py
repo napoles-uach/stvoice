da21c52906abe51b1c868465e9a04f1eb2ba30de
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-05-18 11:03:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -33,55 +33,16 @@ from streamlit_avatar import avatar
 import replicate
 import json
 
-lang = "en-EN"
-state = st.session_state
+import replicate
 
-if 'text_received' not in state:
-    state.text_received = []
-if 'llm_response' not in state:
-    state.llm_response = ""
+input = {
+    "prompt": "Write fizz buzz in SQL",
+    "temperature": 0.2
+}
 
-# Columnas para interfaz
-c1, c2 = st.columns(2)
-with c1:
-    st.write("Convert speech to text:")
-with c2:
-    text = speech_to_text(language='en', use_container_width=True, just_once=True, key='STT')
-
-# Procesar el texto recibido
-if text:
-    state.text_received.append(text)
-    avatar(text)  # Mostrar el avatar diciendo lo que acabas de decir
-
-    # Enviar el texto al LLM
-    input = {
-        "prompt": text,
-        "temperature": 0.2
-    }
-    
-    llm_response = ""
-    try:
-        for event in replicate.stream(
-            "snowflake/snowflake-arctic-instruct",
-            input=input
-        ):
-            if event.data.strip():  # Verificar que event.data no esté vacío
-                event_data = json.loads(event.data)
-                llm_response += event_data.get('text', '')
-    except Exception as e:
-        st.error(f"Error interacting with LLM: {e}")
-    
-    state.llm_response = llm_response
-
-# Mostrar el texto recibido y la respuesta del LLM
-st.write("Tus frases:")
-for text in state.text_received:
-    st.text(text)
-
-st.write("Respuesta del LLM:")
-st.text(state.llm_response)
-
-# Mostrar el avatar diciendo la respuesta del LLM
-if state.llm_response:
-    avatar(state.llm_response, lang=lang)
-
+for event in replicate.stream(
+    "snowflake/snowflake-arctic-instruct",
+    input=input
+):
+    stwrite(event)#, end="")
+#=> "Fizz Buzz is a common programming problem that involves ...
