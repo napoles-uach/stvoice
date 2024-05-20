23e94c775578cc6e4a35df36eeb9382efacab1c0
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-05-19 18:26:43 -0600

    Update snow.py

--- snow.py
+++ snow.py
@@ -5,6 +5,17 @@ from transformers import AutoTokenizer
 from streamlit_mic_recorder import mic_recorder, speech_to_text
 import streamlit.components.v1 as components
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 # Avatar animation function
 def avatar(text='', lang='en-US'):
     try:
